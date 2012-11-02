3a8943db225769beffb36489f1edfcd6828609dc
user <Administrator@127.0.0.1>
2012-11-03 02:51:27 +0900

    First

---
+++ src/tsuttsu305/Main/Main.java
@@ -0,0 +1,31 @@
+package tsuttsu305.Main;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+	public static Main plugin;
+	
+	
+	
+	Logger logger = Logger.getLogger("Minecraft");
+
+	public void onEnable()
+	{
+		PluginDescriptionFile pdfFile = getDescription();
+		this.logger.info(pdfFile.getName() + "version" + pdfFile.getVersion() + " is Enabled");
+		getServer().getPluginManager().registerEvents(new Event(), this);
+		
+	}
+
+	public void onDisable()
+	{
+		PluginDescriptionFile pdfFile = getDescription();
+		this.logger.info(pdfFile.getName() + "version" + pdfFile.getVersion() + " is Disabled");
+	}
+
+	
+}
